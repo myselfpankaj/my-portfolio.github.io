a4786da8367fa029512fc81b7c953de71c616376
Pankaj Kumar Tanwar <pankajku94@gmail.com>
2022-05-23 22:32:51 +0530

    commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -6,3 +6,15 @@ $(function() {
     }
   });
 });
+
+function loadProjects() {
+  
+  $.getJSON('./assets/json/projects.json', function(jd) {
+    var pro = "";
+    jd.forEach(function(item, index) {
+      pro += '<div class="col-lg-4"><div class="pbx" data-aos="fade-up"><a href="'+item.link+'" target="_blank" class="link"><div class="imgbx" style="--image-path:url(../../assets/img/projects/'+item.banner+')"></div><div class="dbx"><h6 class="t">'+item.type+'</h6><h4 class="d">'+item.title+'</h4></div></a></div></div>';
+    });
+    $('#projectsRow').append(pro);
+  });
+
+}
