eea283338e65019d3224d3f1647795d5cdbd16ee
TESTING BAAP <testingkabaap@gmail.com>
2022-05-24 11:58:30 +0530

    commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -11,7 +11,7 @@ function loadProjects() {
   $.getJSON('./assets/json/projects.json', function(jd) {
     var pro = "";
     jd.forEach(function(item, index) {
-      pro += '<div class="col-lg-4" data-aos="flip-up" data-aos-duration="3000"><div class="pbx" ><a href="'+item.link+'" target="_blank" class="link"><div class="imgbx" style="--image-path:url(../../assets/img/projects/'+item.banner+')"></div><div class="dbx"><h6 class="t">'+item.type+'</h6><h4 class="d">'+item.title+'</h4></div></a></div></div>';
+      pro += '<div class="col-md-4" data-aos="flip-up" data-aos-duration="3000"><div class="pbx" ><a href="'+item.link+'" target="_blank" class="link"><div class="imgbx" style="--image-path:url(../../assets/img/projects/'+item.banner+')"></div><div class="dbx"><h6 class="t">'+item.type+'</h6><h4 class="d">'+item.title+'</h4></div></a></div></div>';
       if(index > 2) return true;
     });
     $('#projectsRow').append(pro);
