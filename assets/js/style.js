83f96dc26af0a9a65493cb029c61c3762fc28f6d
TESTING BAAP <testingkabaap@gmail.com>
2022-05-24 11:44:38 +0530

    commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -8,13 +8,22 @@ $(function() {
 });
 
 function loadProjects() {
-  
   $.getJSON('./assets/json/projects.json', function(jd) {
     var pro = "";
     jd.forEach(function(item, index) {
       pro += '<div class="col-lg-4" data-aos="flip-up" data-aos-duration="3000"><div class="pbx" ><a href="'+item.link+'" target="_blank" class="link"><div class="imgbx" style="--image-path:url(../../assets/img/projects/'+item.banner+')"></div><div class="dbx"><h6 class="t">'+item.type+'</h6><h4 class="d">'+item.title+'</h4></div></a></div></div>';
+      if(index > 2) return true;
     });
     $('#projectsRow').append(pro);
   });
+}
 
+function loadSkills(){
+  $.getJSON('./assets/json/skills.json', function(jd) {
+    var skills = "";
+    jd.forEach(function(item, index) {
+      skills += '<div class="skill-bx" title="'+item.title+'"><a href="'+item.link+'" class="link" target="_blank"><div class="skill-pro" role="progressbar" aria-valuenow="'+item.skill+'" aria-valuemin="0" aria-valuemax="100" style="--value:'+item.skill+'"><img src="./assets/img/icon/'+item.img+'" class="icon"><div class="stitle">'+item.title+'</div></div></a></div>';
+    });
+    $('#allSkills').append(skills);
+  });
 }
