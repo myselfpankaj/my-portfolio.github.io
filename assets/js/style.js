9858548771537ea0abca56e58e5abfb089ad30c7
TESTING BAAP <testingkabaap@gmail.com>
2022-05-24 15:20:59 +0530

    commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -9,31 +9,28 @@ $(function() {
 
 function loadProjects() {
   $.getJSON('./assets/json/projects.json', function(jd) {
-    var pro = "";
     jd.forEach(function(item, index) {
-      pro += '<div class="col-md-4" data-aos="flip-up" data-aos-duration="3000"><div class="pbx" ><a href="'+item.link+'" target="_blank" class="link"><div class="imgbx" style="--image-path:url(../../assets/img/projects/'+item.banner+')"></div><div class="dbx"><h6 class="t">'+item.type+'</h6><h4 class="d">'+item.title+'</h4></div></a></div></div>';
-      if(index > 2) return true;
+      if(index > 2) {return true;}
+      var pro = '<div class="col-md-4" data-aos="flip-up" data-aos-duration="3000"><div class="pbx" ><a href="'+item.link+'" target="_blank" class="link"><div class="imgbx" style="--image-path:url(../../assets/img/projects/'+item.banner+')"></div><div class="dbx"><h6 class="t">'+item.type+'</h6><h4 class="d">'+item.title+'</h4></div></a></div></div>';
+      $('#projectsRow').append(pro);
     });
-    $('#projectsRow').append(pro);
   });
 }
 
 function loadSkills(){
   $.getJSON('./assets/json/skills.json', function(jd) {
-    var skills = "";
     jd.forEach(function(item, index) {
-      skills += '<div class="skill-bx" title="'+item.title+'"><a href="'+item.link+'" class="link" target="_blank"><div class="skill-pro" role="progressbar" aria-valuenow="'+item.skill+'" aria-valuemin="0" aria-valuemax="100" style="--value:'+item.skill+'"><img src="./assets/img/icon/'+item.img+'" class="icon"><div class="stitle">'+item.title+'</div></div></a></div>';
+      var skills = '<div class="skill-bx" title="'+item.title+'"><a href="'+item.link+'" class="link" target="_blank"><div class="skill-pro" role="progressbar" aria-valuenow="'+item.skill+'" aria-valuemin="0" aria-valuemax="100" style="--value:'+item.skill+'"><img src="./assets/img/icon/'+item.img+'" class="icon"><div class="stitle">'+item.title+'</div></div></a></div>';
+      $('#allSkills').append(skills);
     });
-    $('#allSkills').append(skills);
   });
 }
 
 function loadSociallLinks(){
   $.getJSON('./assets/json/social-links.json', function(jd) {
-    var social = "";
     jd.forEach(function(item, index) {
-      social += '<a href="#" class="link" target="_blank" aria-label="'+item.title+'" rel="noreferrer">'+item.icon+'</a>';
+      var social = '<a href="'+item.link+'" class="link" target="_blank" aria-label="'+item.title+'" rel="noreferrer">'+item.icon+'</a>';
+      $('#footerAllSocialLinks').append(social);
     });
-    $('#footerAllSocialLinks').append(social);
   });
 }
